ae8ff4541478d1d26987c8d3ad3fb3638e693a85
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-24 12:09:44 +0300

    Initial Commit

---
+++ Contact List/Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_ListApp.swift
+//  Contact List
+//
+//  Created by John Doe on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
